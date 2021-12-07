cd844d64487332e4dfa0226117976a2574cf65a0
Derek Reimanis <derek.reimanis@gmail.com>
2021-12-07 10:57:15 -0700

    transition to single infrastructure objects

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import uuid
 
 import stix2
+from taxii2client import Collection
 
 
 def loadJson(name):
@@ -20,15 +21,30 @@ def loadJson(name):
     infrastructureObj = parseInfrastructure(infrastructure)
     diagObjects = parseDiagnostics(diagnostics)
     measureObjects = parseMeasures(measures)
+    pfObjects = parseProductFactors(product_factors)
+
+    infrastructureToDiagnostics = buildInfrastructureRelationship(diagObjects, infrastructureObj, "has")
+
 
     diagnosticsToMeasures = buildRelationship(diagObjects, measures, measureObjects, "derived-from")
 
-    stixObjects = infrastructureObj + diagObjects + measureObjects + diagnosticsToMeasures
+    #not quite working....
+    measuresToProductFactors = buildRelationship(measureObjects, product_factors, pfObjects, "derived-from")
+
+    stixObjects = infrastructureObj + diagObjects + measureObjects + diagnosticsToMeasures + measuresToProductFactors
 
     buildBundle(out, stixObjects)
     out.close()
 
-def buildRelationship(lower_stix, upper_pique, upper_stix, relationshipKey):
+
+def buildInfrastructureRelationship(lower_stix, infrastructure, relationship_type):
+    relationships = []
+    for lower_stix_obj in lower_stix:
+        relationships.append(buildRelationshipStix(infrastructure, lower_stix_obj, relationship_type))
+
+    return relationships
+
+def buildRelationship(lower_stix, upper_pique, upper_stix, relationship_type):
     relationships = []
     for upper in upper_pique:
         lower_stix_find = None
@@ -46,19 +62,18 @@ def buildRelationship(lower_stix, upper_pique, upper_stix, relationshipKey):
                 if weights == lower.name:
                     lower_stix_find = lower
 
-        relationships.append(buildRelationshipStix(lower_stix_find, upper_stix_find))
+        if lower_stix_find is not None and upper_stix_find is not None:
+            relationships.append(buildRelationshipStix(lower_stix_find, upper_stix_find, relationship_type))
     return relationships
 
-def buildRelationshipStix(lower_stix, upper_stix):
+def buildRelationshipStix(lower_stix, upper_stix, relationship_type):
     return stix2.v21.Relationship(
         type="relationship",
-        relationship_type="indicates",
+        relationship_type=relationship_type,
         source_ref=lower_stix.id,
         target_ref=upper_stix.id,
         id='relationship--' + str(uuid.uuid4()),
     )
-
-    return relationships
 
 def buildBundle(out, stixObjects):
     bundle = stix2.v21.Bundle(
@@ -82,6 +97,14 @@ def parseInfrastructure(infrastructure):
     return infrastructureArray
 
 
+def parseProductFactors(product_factors):
+    stixpackage = []
+    for i in product_factors:
+        if 'CWE' in product_factors[i]['name']:
+            #calling everything a cwe vuln at this level
+            stixpackage.append(buildStixVulnerability(product_factors[i]))
+
+    return stixpackage
 
 def parseMeasures(measures):
     stixpackage = []
@@ -110,6 +133,10 @@ def parseDiagnostics(diagnostics):
 
 
 def buildStixVulnerability(diagnostic):
+    #check to see if vulnerability exists already
+    #fs = stix2.FileSystemSource('./cti-stix-common-objects/objects')
+    #filter = stix2.Filter('type', '=', 'vulnerability')
+    #vulnerabilities = fs.query([filter])
     vulnerability = stix2.v21.Vulnerability(
         # uuid4 instructions from https://docs.oasis-open.org/cti/stix/v2.1/os/stix-v2.1-os.html#_q5ytzmajn6re
         id='vulnerability--'+str(uuid.uuid4()),
@@ -125,11 +152,24 @@ def buildStixIndicator(diagnostic):
     )
     return indicator
 
+#not used until I can find a filesystem store of common stix objects.
+def loadCommonObjects():
+    # create FileSystemStore
+    fs = stix2.FileSystemSource("cti-stix-common-objects/objects")
 
+    # create TAXIICollectionSource -- Im guessing there is no standard repo for this.
+    colxn = Collection('http://127.0.0.1:5000/trustgroup1/collections/91a7b528-80eb-42ed-a74d-c6fbd5a26116/',
+                       user="user1", password="Password1")
+    ts = stix2.TAXIICollectionSource(colxn)
+
+    # add them both to the CompositeDataSource
+    cs = stix2.CompositeDataSource()
+    cs.add_data_sources([fs, ts])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    #loadCommonObjects()
     loadJson('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
